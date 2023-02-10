d2f8632939a8ff479390e084e62ac99e2e32eb7d
Steven <steven_padilla728@outlook.com>
2023-02-10 08:19:16 -0600

    Algoritmo funcionando

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ class Neurona():
         self.eta = eta
         self.cantidad_x = cantidad_x
         self.error_permisible = error_permisible
-        self.lista_x = numpy.array([[651.92, 645.61, -990.08],
-                        [-804.69, 333.05, 682.68],
-                        [-220.11, -333.26, 780.61],
-                        [-337.46, 616.51, -864.44],
-                        [257.56, 507.27, -774.94]])
+        self.lista_x = numpy.array([[1,651.92, 645.61, -990.08],
+                        [1,-804.69, 333.05, 682.68],
+                        [1,-220.11, -333.26, 780.61],
+                        [1,-337.46, 616.51, -864.44],
+                        [1,257.56, 507.27, -774.94]])
         self.y_deseada = numpy.array([-6914.52, 1783.1, 4052, -4545.16, -4937.24])
         self.lista_w = self.generar_w()
         self.lista_e=[]
@@ -21,10 +21,15 @@ class Neurona():
         self.err_nuevo=0
         self.iniciar_alg()
         
-        while self.err_nuevo > self.error_permisible and self.err_viejo <= self.err_nuevo:
-            print(f'error nuevo: {self.err_nuevo}')
-            print(f'error viejo: {self.err_viejo}')
-            self.iniciar_alg()
+        while self.err_nuevo > self.error_permisible :
+            if self.err_viejo ==0  :
+                self.iniciar_alg()
+            elif self.err_viejo > self.err_nuevo:
+                self.iniciar_alg()
+            else:
+                break
+            
+
         graficar_error(self.lista_e,self.lista_iter)
 
     def iniciar_alg(self):
@@ -32,8 +37,10 @@ class Neurona():
         self.y_calculada=self.funcion_activacion()
         self.error = self.calcular_error()
         self.lista_w=self.nueva_w()
-        self.lista_iter.append(self.aux+1)
+        self.lista_iter.append(self.aux)
         self.aux += 1
+        print(f'error nuevo: {self.err_nuevo}')
+        print(f'error viejo: {self.err_viejo}')
         
     def generar_w(self):
         lista_w = []
@@ -51,16 +58,16 @@ class Neurona():
         return self.lista_u
 
     def calcular_error(self):
-        e = numpy.subtract(self.y_deseada, self.y_calculada)
+        e = numpy.subtract(self.y_deseada,self.y_calculada)
         self.err_viejo=self.err_nuevo #0
-        self.err_nuevo=numpy.linalg.norm(e) / len(self.lista_x) #error random
+        self.err_nuevo=numpy.linalg.norm(e)  #error random
         self.lista_e.append(self.err_nuevo)
         return e
 
     def nueva_w(self):
         e_x = numpy.matmul(numpy.transpose(self.error), self.lista_x)
         n_ex = numpy.dot(self.eta,e_x) 
-        nueva_w = self.lista_w-n_ex
+        nueva_w = self.lista_w+n_ex
         print(f'Lista u: {self.lista_u}')
         return nueva_w
 
@@ -69,4 +76,4 @@ class Neurona():
 if __name__ == '__main__':
     data=Interfaz().get_datos()
 
-    algotirmo = Neurona(eta=data['aprendizaje'], cantidad_x=3,error_permisible=data['margen_error'])
+    algotirmo = Neurona(eta=data['aprendizaje'], cantidad_x=4,error_permisible=data['margen_error'])
