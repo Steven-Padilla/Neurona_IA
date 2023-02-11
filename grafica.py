7cb2b1ce26fab63d224c8fd2338bf62a6b17cc4f
Steven-Padilla <203426@ids.upchiapas.edu.mx>
2023-02-11 11:59:39 -0600

    Graficas arregladas y maximo error observado agregado en reporte

--- grafica.py
+++ grafica.py
@@ -4,12 +4,12 @@ import csv
 
 def graficar_error(error,valor_x):
     fig, ax = plt.subplots()
+    ax.set_title('Gráfica evolución del error')
     ax.set_xlabel('Iteración')
     ax.set_ylabel('Magnitud del error')
     ax.plot(valor_x, error,label='Error')
     ax.legend(loc='upper right')
     plt.savefig('Evolución_error')
-    # plt.show()
 
 def graficar_evolucion_pesos(pesos,valor_x):
     list1,list2,list3,list4 = [[],[],[],[]]
@@ -19,6 +19,7 @@ def graficar_evolucion_pesos(pesos,valor_x):
         list3.append(peso[2])
         list4.append(peso[3])
     fig,ax = plt.subplots()
+    ax.set_title('Gráfica evolución de los pesos')
     ax.set_xlabel('Iteración')
     ax.set_ylabel('Evolucion de pesos')
     ax.plot(valor_x, list1, marker='.', label = 'W0')
@@ -31,16 +32,17 @@ def graficar_evolucion_pesos(pesos,valor_x):
 def graficar_versus(y_deseada,y_calculada):
     valor_x = range(1,len(y_deseada)+1)    
     fig, ax = plt.subplots()
+    ax.set_title('Y deseada y Y calculada final')
     ax.plot(valor_x, y_deseada,color = "green",marker = "x", label="Y Deseada")
     ax.plot(valor_x, y_calculada,color = "red",marker = "o", label="Y Calculada")
     ax.legend(loc="upper right")
     plt.savefig('Comparacion Ys')
-    # plt.show()
 
 def graficar_error_versus(error,valor_x):
     identificador = range(1,len(valor_x)+1)
     error_inicial,error_final=[error[0],error[len(error)-1]]
     fig, ax = plt.subplots()
+    ax.set_title('Error inicial y Error final')
     ax.set_xlabel('ID')
     ax.set_ylabel('Error Observado')
     ax.plot(identificador, error_inicial,label='Error Inicial')
@@ -48,15 +50,13 @@ def graficar_error_versus(error,valor_x):
 
     ax.legend(loc='upper right')
     plt.savefig('Error_versus')
-    # plt.show()
 
 def reporte(pesos,error_permisible,lista_e_observado, iteraciones):
-    # 'Max eObservado',
-    titulos=['Pesos iniciales', 'Pesos finales','Error permisible','Error observado','Epocas entrenamiento']
+    titulos=['Pesos iniciales', 'Pesos finales','Error permisible','Error observado','Epocas entrenamiento','Max Error observado']
     print(*lista_e_observado,sep='\n')
     with open('reporte.csv', 'w', newline='') as csvfile:
         wr = csv.DictWriter(csvfile, quoting=csv.QUOTE_ALL,fieldnames=titulos)
-        # 'Max eObservado':max(lista_e_observado),
-        dic_print=[{'Pesos iniciales':pesos[0],'Pesos finales':pesos[len(pesos)-1],'Error permisible':error_permisible,'Error observado':lista_e_observado[len(lista_e_observado)-1],'Epocas entrenamiento':iteraciones}]
+        dic_print=[{'Pesos iniciales':pesos[0],'Pesos finales':pesos[len(pesos)-1],'Error permisible':error_permisible,
+            'Error observado':lista_e_observado[len(lista_e_observado)-2],'Epocas entrenamiento':iteraciones,'Max Error observado':max(lista_e_observado)}]
         wr.writeheader()
         wr.writerows(dic_print)
